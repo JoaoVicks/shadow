503f1733ec69954976f99aa837cee040c18af6c0
João Pedro Vitorio Santos Souza <132296221+JoaoVicks@users.noreply.github.com>
2024-04-20 19:50:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,18 +19,35 @@ const spanBoxShadow = document.querySelector('#box-shadow span')
 const spanWebkitBoxShadow = document.querySelector('#-webkit-box-shadow span')
 const spanMsBoxShadow = document.querySelector('#-ms-box-shadow span')
 
+const inputColorShadow = document.querySelector('#iColorShadow')
+const visuColorShadow = document.querySelector('#visualizacao-colorShadow')
+
+const inputColorBox = document.querySelector('#iColorBox')
+const visuColorBox = document.querySelector('#visualizacao-colorBox')
+
+const inputsColor = document.querySelectorAll(`input[type="color"]`)
+
 
 // -------------------------------------------------//
 class BoxShadow {
-    constructor(inputHorizontal, visuHorizontal, inputVertical, visuVertical, inputBlur, visuBlur, inputSpread, visuSpread) {
+    constructor(inputHorizontal, visuHorizontal, inputVertical, visuVertical, inputBlur, visuBlur, inputSpread, visuSpread, inputColorBox, visuColorBox, inputColorShadow, visuColorShadow) {
         this.inputHorizontal = inputHorizontal,
             this.visuHorizontal = visuHorizontal,
+
             this.inputVertical = inputVertical,
             this.visuVertical = visuVertical,
+
             this.inputBlur = inputBlur,
             this.visuBlur = visuBlur,
+
             this.inputSpread = inputSpread,
-            this.visuSpread = visuSpread
+            this.visuSpread = visuSpread,
+
+            this.inputColorShadow = inputColorShadow,
+            this.visuColorShadow = visuColorShadow,
+
+            this.inputColorBox = inputColorBox,
+            this.visuColorBox = visuColorBox
     }
     inicializacao() {
 
@@ -40,23 +57,42 @@ class BoxShadow {
         this.visuSpread.value = this.inputSpread.value
     }
 
-    Atualizacao(nmInput) {
+    Atualizacao(nmInput, nmInputColor = false) {
 
-        switch (nmInput) {
-            case 'nmHorizontal':
-                this.visuHorizontal.value = this.inputHorizontal.value
-                break
-            case 'nmVertical':
-                this.visuVertical.value = this.inputVertical.value
-                break
-            case 'nmBlur':
-                this.visuBlur.value = this.inputBlur.value
-                break
-            case 'nmSpread':
-                this.visuSpread.value = this.inputSpread.value
-                break
+        if (nmInput !== false) {
+
+            switch (nmInput) {
+                case 'nmHorizontal':
+                    this.visuHorizontal.value = this.inputHorizontal.value
+                    break
+                case 'nmVertical':
+                    this.visuVertical.value = this.inputVertical.value
+                    break
+                case 'nmBlur':
+                    this.visuBlur.value = this.inputBlur.value
+                    break
+                case 'nmSpread':
+                    this.visuSpread.value = this.inputSpread.value
+                    break
+
+            }
         }
-    this.AtualizacaoRules()
+
+        if (nmInputColor !== false) {
+            switch (nmInputColor) {
+                case 'nmColorShadow':
+                    this.visuColorShadow.value = this.inputColorShadow.value
+                    break
+
+                case 'nmColorBox':
+                    this.visuColorBox.value = this.inputColorBox.value
+                    break
+            }
+        }
+
+
+
+        this.AtualizacaoRules()
     }
 
     targetInputRange(e, input) {
@@ -67,22 +103,44 @@ class BoxShadow {
             #fff ${valor}%, #fff 100%)`;
     }
 
-    AtualizacaoRules(){
+    AtualizacaoRules() {
+
         const horizontal = this.inputHorizontal.value;
         const vertical = this.inputVertical.value;
         const blur = this.inputBlur.value;
         const spread = this.inputSpread.value;
+        const colorShadow = this.inputColorShadow.value
+        const colorBox = this.inputColorBox.value
 
-        spanBoxShadow.innerText= `${horizontal}px ${vertical}px ${blur}px ${spread}px`
-        spanWebkitBoxShadow.innerText = `${horizontal}px ${vertical}px ${blur}px ${spread}px`
-        spanMsBoxShadow.innerText = `${horizontal}px ${vertical}px ${blur}px ${spread}px`
-        
-        viewBox.style.boxShadow = `${horizontal}px ${vertical}px ${blur}px ${spread}px`;
+        viewBox.style.boxShadow = `${colorShadow} ${horizontal}px ${vertical}px ${blur}px ${spread}px`;
+        viewBox.style.backgroundColor = colorBox
+
+        spanBoxShadow.innerText = `${colorShadow} ${horizontal}px ${vertical}px ${blur}px ${spread}px`
+        spanWebkitBoxShadow.innerText = `${colorShadow} ${horizontal}px ${vertical}px ${blur}px ${spread}px`
+        spanMsBoxShadow.innerText = `${colorShadow} ${horizontal}px ${vertical}px ${blur}px ${spread}px`
+
+
     }
 }
-const box = new BoxShadow(inputHorizontal, visuHorizontal, inputVertical, visuVertical, inputBlur, visuBlur, inputSpread, visuSpread)
+// Criação do objeto derivado da classe BoxShadow --------------------------------------------------------
+
+const box = new BoxShadow(
+    inputHorizontal,
+    visuHorizontal,
+    inputVertical,
+    visuVertical,
+    inputBlur,
+    visuBlur,
+    inputSpread,
+    visuSpread,
+    inputColorBox,
+    visuColorBox,
+    inputColorShadow,
+    visuColorShadow
+)
 
 box.inicializacao()
+
 // Eventos ------------------------------------------//
 inputsRange.forEach((input) => {
 
@@ -94,4 +152,18 @@ inputsRange.forEach((input) => {
     })
 
 })
+inputsColor.forEach((inputColor) => {
+
+    inputColor.addEventListener('input', (e) => {
+
+        box.Atualizacao(false, inputColor.name);
+    });
+    // Aqui estava faltando um parêntese para fechar a função de callback
+
+}
+
+)
+
+
+
 
